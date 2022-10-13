075edf889591390bd64a87043f90660366eba4ad
Theofanis Markos <fanismarkos1998@gmail.com>
2022-10-13 10:24:33 +0300

    Add Url to Enviroment for FE

--- FE/ClientApp/src/app/user.service.ts
+++ FE/ClientApp/src/app/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { User } from './user-list/user-list.component';
 
 @Injectable({
@@ -8,30 +9,28 @@ import { User } from './user-list/user-list.component';
 })
 export class UserService {
 
-  constructor(private http : HttpClient) { }
+  public apiUrl: string = environment.domain;
+  constructor(private http: HttpClient) { }
 
-  getUsers(): Observable<User[]>
-  {
-     return this.http.get<User[]>("https://localhost:7193/api/User")
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl + "api/User")
   }
 
-  createUser(data :User):Observable<void>{
+  createUser(data: User): Observable<void> {
 
-    return this.http.post<void>("https://localhost:7193/api/User/Create",data)
+    return this.http.post<void>(this.apiUrl + "api/User/Create", data)
   }
 
-  getUserByid(id:number):Observable<User>
-  {
-     return this.http.get<User>(`https://localhost:7193/api/User/${id}`)
+  getUserByid(id: number): Observable<User> {
+    return this.http.get<User>(this.apiUrl + `api/User/${id}`)
   }
 
-  updateUser(data :User):Observable<void>
-  {
-    return this.http.put<void>(`https://localhost:7193/api/User/Update/${data.id}`,data)
+  updateUser(data: User): Observable<void> {
+    return this.http.put<void>(this.apiUrl + `api/User/Update/${data.id}`, data)
   }
 
-  deleteUser(id:number|undefined):Observable<void>
-  {
-    return this.http.delete<void>(`https://localhost:7193/api/User/Delete/${id}`)
+  deleteUser(id: number | undefined): Observable<void> {
+    return this.http.delete<void>(this.apiUrl + `api/User/Delete/${id}`)
   }
 }
